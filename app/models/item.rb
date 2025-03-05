c54945d00381b2e5552da63917e64dca13a65a70
Genius Chakurira <geniusmaud01@hotmail.com>
2025-03-05 16:29:48 +0200

    added the bookings model and did some styling

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_many :bookings
   validates :name, :category, :description, :price, presence: true
   validates :category, inclusion: { in: %w[event_set_up sound_system] }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # get 'bookings/index'
+  # get 'bookings/create'
+  # get 'bookings/edit'
+  # get 'bookings/update'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,6 +13,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :items, only: %i[index show new create]
-
+  resources :items, only: %i[index show new create] do
+    resources :bookings
+  end
+  # resources :bookings, only: %i[index]
 end
