4908379567655879629f39099b6b5f5008f9da59
SizaMpafa <sizampafa972@gmail.com>
2025-03-06 11:37:13 +0200

    Now I have a section where I can see all my bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,39 +1,27 @@
 class BookingsController < ApplicationController
   def index
-    @item = Item.find(params[:item_id])
-    @bookings = Booking.where(item: @item)
+    @bookings = Booking.where(user: current_user)
   end
-
   def show
     @booking = Booking.find(params[:id])
   end
-
   def new
     @booking = Booking.new
   end
-
   def create
     @item = Item.find(params[:item_id])
     @booking = Booking.new(booking_params)
     @booking.item_id = @item.id
     @booking.user = current_user
-
     if @booking.save
-      redirect_to item_bookings_path(@item)
+      redirect_to bookings_path
     else
       render :items/show, status: :unprocessable_entity
     end
   end
-
-  def edit
-  end
-
-  def update
-  end
-
   private
-
   def booking_params
     params.require(:booking).permit(:date)
   end
 end
+
